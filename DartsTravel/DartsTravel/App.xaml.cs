b26b826171521674cc59a3aba1ab0cdea98ae1ea
Okuyama Haruto <oku127haru0127@gmail.com>
2024-06-07 13:38:09 +0900

    メニューページを追加

--- DartsTravel/DartsTravel/App.xaml.cs
+++ DartsTravel/DartsTravel/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DartsTravel
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MenuPage();
         }
 
         protected override void OnStart()
---
+++ DartsTravel/DartsTravel/MenuPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DartsTravel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+            dartskamae.Source = ImageSource.FromResource("DartsTravel.Image.dartskamae.jpg");
+        }
+
+        void OnTap(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
